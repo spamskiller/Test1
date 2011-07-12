494c9e137725652693ff297643fd0b1ad0756134
DatVT <dat@dev.org>
2011-07-12 16:56:25 +0800

    Test4
    Signed-off-by: DatVT <dat@dev.org>

--- Test1/src/com/dat/HelloWorld.java
+++ Test1/src/com/dat/HelloWorld.java
@@ -6,8 +6,17 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test"); 
+		HelloWorld hello = new HelloWorld();
+		hello.test("bebe");
 
 	}
+	
+	public void test(String str) {
+		System.out.println("show text: " + str);
+	}
 
+	public void showMessage() {
+		System.out.println("alo");
+	}
 }
