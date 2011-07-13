cd3100ca58f6bc6148487e5e50184dcb0dca1e0b
DatVT <dat@dev.org>
2011-07-13 12:04:11 +0800

    sa

--- Test1/src/com/dat/HelloWorld.java
+++ Test1/src/com/dat/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
 		System.out.println("show 111: " + str);
 	}
 
-	public void showMessage1aaaaaaaaaaxxx() {
+	public void showMessage1aaaaaaaaaaxxxyyyyy() {
 		System.out.println("alo1xxxxxxxxx");
 	}
 }
